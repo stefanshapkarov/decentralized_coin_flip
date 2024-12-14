679db3a8d063788a8288f4c47481998ceeefe69e
stefanshapkarov <stefannshapkarov@gmail.com>
2024-12-14 21:54:27 +0100

    added a configuration for deployment on sepolia including dotenv, deployed a version on the testnet and started working on frontend app

--- Contract/hardhat.config.js
+++ Contract/hardhat.config.js
@@ -1,7 +1,26 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-ignition");
+require('dotenv').config()
+
+// Go to https://infura.io, sign up, create a new API key
+// in its dashboard, and add it to the configuration variables
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+
+// Add your Sepolia account private key to the configuration variables
+// To export your private key from Coinbase Wallet, go to
+// Settings > Developer Settings > Show private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY.toString();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,30 +1,40 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue';
+import { ethers } from 'ethers';
+
+const isLoggedIn = ref(false);
+const userAddress = ref('');
+const balance = ref(0);
+
+async function connectMetaMask() {
+  let provider;
+  let account;
+  let unformattedBalance;
+  if (window.ethereum && window.ethereum.isMetaMask) {
+    provider = new ethers.BrowserProvider(window.ethereum);
+    account = await provider.getSigner();
+    
+    userAddress.value = account.address;
+    unformattedBalance = await provider.getBalance(userAddress.value);
+    balance.value = ethers.formatEther(unformattedBalance);
+    isLoggedIn.value = true;
+  } else {
+    alert('MetaMask is not installed');
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="container">
+    <div v-if="isLoggedIn">
+      <h3>Account: {{ userAddress }}</h3>
+      <h3>Balance: {{ balance }}</h3>
+    </div>
+    <div v-else>
+      <button @click="connectMetaMask">Connect to MetaMask</button>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
