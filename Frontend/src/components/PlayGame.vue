de82d1b44f38c6fefbedbdbaf88de333868ecd08
stefanshapkarov <stefannshapkarov@gmail.com>
2024-12-16 23:42:08 +0100

    finished rest of the game functionalities (flipping coin, cancelling game)

--- Frontend/src/components/PlayGame.vue
+++ Frontend/src/components/PlayGame.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, inject, watchEffect, ref } from 'vue';
+import { computed, inject, watchEffect, ref } from 'vue';
 import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 
 const GameStatus = {
@@ -16,15 +16,30 @@ const gameId = JSON.parse(props.gameId);
 const contractRead = inject('contractRead');
 const contractWrite = inject('contractWrite');
 const currentBlockTimestamp = inject('currentBlockTimestamp');
+const isGameCancellable = computed(() => {
+    if (!gameData.value || !currentBlockTimestamp.value) {
+        return false;
+    }
+    
+    const gameCreationTime = Number(gameData.value.createdAt);
+    const currentTime = Number(currentBlockTimestamp.value);
+    
+    return (
+        gameData.value.status == GameStatus.INITIALIZED &&
+        currentTime >= (gameCreationTime + 86400)
+    );
+});
 const userAddress = inject('userAddress');
+const updateBalance = inject('updateBalance');
 const isLoading = ref(true);
 const gameData = ref(null);
+const selectedSide = ref(null);
 
 async function loadGame() {
     try {
         const game = await contractRead.value.allGames(gameId);
         gameData.value = game;
-
+        
         isLoading.value = false;
     } catch (error) {
         alert('Error loading the game: ' + error.message);
@@ -36,20 +51,51 @@ async function acceptGame() {
         isLoading.value = true;
 
         const transaction = await contractWrite.value.acceptGame(gameId, { value: gameData.value.betAmount });
-        const receipt = await transaction.wait();
-
-        const gameAcceptedEvent = receipt.logs.find(
-            (log) => log.fragment && log.fragment.name === 'GameAccepted'
-        );
-
-        if (gameAcceptedEvent) {
-            const { gameId, pickingSide } = gameAcceptedEvent.args;
-            console.log("Game with id: " + gameId + " is accepted, picking side: " + pickingSide);
-        }
-
-        loadGame();
+        await transaction.wait();
     } catch (error) {
         alert("Can't accept game: " + error.message);
+    } finally {
+        loadGame();
+        updateBalance();
+    }
+}
+
+async function cancelGame() {
+    try {
+        isLoading.value = true;
+
+        const transaction = await contractWrite.value.cancelGame(gameId);
+        await transaction.wait();
+    } catch (error) {
+        alert("Can't cancel the game: " + error.message);
+    } finally {
+        loadGame();
+        updateBalance();
+    }
+}
+
+async function flipCoin() {
+    try {
+        isLoading.value = true;
+        
+        if (!selectedSide.value) {
+            alert("Please select a side first");
+            return;
+        }
+
+        const pickingSide = parseInt(selectedSide.value);
+        if (pickingSide !== 1 && pickingSide !== 2) {
+            alert("Invalid coin side.");
+            return;
+        }
+
+        const transaction = await contractWrite.value.flipCoin(gameId, pickingSide);
+        await transaction.wait();
+    } catch (error) {
+        alert("Can't flip coin: " + error.message);
+    } finally {
+        loadGame();
+        updateBalance();
     }
 }
 
@@ -95,15 +141,52 @@ watchEffect(async () => {
                 </div>
                 <div v-else>
                     <p>Waiting for opponent to accept the game...</p>
+                    <button v-if="isGameCancellable" @click="cancelGame">Cancel game</button>
                 </div>
             </div>
             <div v-else-if="gameData.status == GameStatus.STARTED">
                 <div v-if="isUserFlipper()">
-                    <button>Flip Coin</button>
+                    <h4>Game started!</h4>
+                    <h6>You are the flipper, flip the coin by pressing the button!</h6>
+                    <form @submit.prevent="flipCoin" id="flip-coin">
+                        <div id="radio-buttons">
+                            <label for="heads">Heads</label>
+                            <input 
+                                type="radio" 
+                                value="1" 
+                                v-model="selectedSide"
+                                id="heads" 
+                                name="coinSide"
+                            />
+                            <label for="tails">Tails</label>
+                            <input 
+                                type="radio" 
+                                value="2" 
+                                v-model="selectedSide"
+                                id="tails" 
+                                name="coinSide"
+                            />
+                        </div>
+
+                        <button type="submit" :disabled="!selectedSide">Flip Coin</button>
+                    </form>
                 </div>
                 <div v-else>
+                    <h4>Game started!</h4>
                     <p>Waiting for flipper to flip the coin...</p>
                 </div>
+            </div>
+            <div v-else-if="gameData.status == GameStatus.FINISHED">
+                <h4>Game finished!</h4>
+                <p v-if="gameData.winner == userAddress" style="color: green">
+                    <b>You are the winner!</b>
+                </p>
+                <p v-else style="color: red;">
+                    <b>Sadly you lost, better luck next time!</b>
+                </p>
+            </div>
+            <div v-else>
+                <h3 style="color: yellow">Game has been cancelled.</h3>
             </div>
         </div>
     </div>
@@ -114,5 +197,11 @@ watchEffect(async () => {
     border: 1px solid grey;
     padding: 10px;
     width: 350px;
+}
+
+#flip-coin {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
 }
 </style>
