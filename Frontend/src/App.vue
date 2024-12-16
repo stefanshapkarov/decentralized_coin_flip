8aeb4c4c1b7942c749edc8ef804ab7c1f7615f52
stefanshapkarov <stefannshapkarov@gmail.com>
2024-12-16 12:58:46 +0100

    abi is now loaded directly from file in frontend folder, finished creating game functionality

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,35 +1,41 @@
 <script setup>
 import { ref, provide, onMounted } from 'vue';
 import { ethers } from 'ethers';
+import CoinFlipABI from './abis/CoinFlipABI.json';
 import CreateGame from './components/CreateGame.vue';
 
 const provider = ref(null);
 const signer = ref(null);
-const contract = ref(null);
+const contractRead = ref(null);
+const contractWrite = ref(null);
 const userAddress = ref(null);
 const balance = ref(0);
 
-async function initializeProvider() {
-  let provider;
-  let signer;
-  let unformattedBalance;
+async function connectMetaMask() {
   if (window.ethereum && window.ethereum.isMetaMask) {
     try {
-      provider = new ethers.BrowserProvider(window.ethereum);
-      signer = await provider.getSigner();
-      provider.value = provider;
-      signer.value = signer;
-      userAddress.value = await signer.getAddress();
-      sessionStorage.setItem('userAddress', userAddress.value);
-      unformattedBalance = await provider.getBalance(userAddress.value);
-      balance.value = ethers.formatEther(unformattedBalance);
-      sessionStorage.setItem('balance', balance.value);
+      const tempProvider = new ethers.BrowserProvider(window.ethereum);
+      const tempSigner = await tempProvider.getSigner();
 
-      contract.value = new ethers.Contract(
+      provider.value = tempProvider;
+      signer.value = tempSigner;
+
+      userAddress.value = await tempSigner.getAddress();
+      const unformattedBalance = await tempProvider.getBalance(userAddress.value);
+      balance.value = ethers.formatEther(unformattedBalance);
+
+      contractRead.value = new ethers.Contract(
         '0x5FbDB2315678afecb367f032d93F642f64180aa3',
-        getAbi('CoinFlip', 31337),
-        provider
+        CoinFlipABI,
+        tempProvider
       );
+
+      contractWrite.value = new ethers.Contract(
+        '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+        CoinFlipABI,
+        tempSigner
+      );
+
     } catch (error) {
       alert('The following error occurred: ' + error.message);
     }
@@ -38,41 +44,22 @@ async function initializeProvider() {
   }
 };
 
-function metaMaskListener() {
-  window.ethereum.on('accountsChanged', async () => {
-    await connectMetaMask();
-  });
-  window.ethereum.on('chainChanged', async () => {
-    await connectMetaMask();
-  });
-}
-
 onMounted(() => {
-  if (userAddress.value == null && balance.value === 0) {
-    userAddress.value = sessionStorage.getItem('userAddress');
-    balance.value = sessionStorage.getItem('balance');
-  }
-  if (userAddress.value) {
-    metaMaskListener();
-  }
+  connectMetaMask();
 });
 
 provide('provider', provider);
 provide('signer', signer);
-provide('contract', contract);
+provide('contractRead', contractRead);
+provide('contractWrite', contractWrite);
 provide('userAddress', userAddress);
 </script>
 
 <template>
   <div id="container">
-    <div v-if="userAddress">
-      <h3>Account: {{ userAddress }}</h3>
-      <h3>Balance: {{ balance }} ETH</h3>
-      <router-view />
-    </div>
-    <div v-else>
-      <button @click="initializeProvider">Connect MetaMask</button>
-    </div>
+    <h3>Account: {{ userAddress }}</h3>
+    <h3>Balance: {{ balance }} ETH</h3>
+    <router-view />
   </div>
 </template>
 
--- Frontend/src/components/CreateGame.vue
+++ Frontend/src/components/CreateGame.vue
@@ -1,16 +1,22 @@
 <script setup>
-import { ref, computed, inject } from 'vue';
+import { ref, computed, inject, onUnmounted } from 'vue';
+import { ethers } from 'ethers';
 
 const opponentAddress = ref('');
 const betAmount = ref('');
-const contract = inject('contract');
-
-const userAddress = computed(() => {
-  return sessionStorage.getItem('userAddress');
-});
+const contractRead = inject('contractRead');
+const contractWrite = inject('contractWrite');
+const inviteUrl = ref('');
 
 async function createGame() {
-  console.log(opponentAddress.value, betAmount.value);
+    try {
+        const transaction = await contractWrite.value.createGame(opponentAddress.value, { value: ethers.parseEther(betAmount.value.toString()) });
+        await transaction.wait();
+
+        console.log(transaction);
+    } catch (error) {
+        alert("The following error occurred: " + error.message);
+    }
 }
 </script>
 
@@ -18,35 +24,41 @@ async function createGame() {
     <div id="form-container">
         <form @submit.prevent="createGame">
             <label for="opponentAddress">Opponent Address</label>
-            <input type="text" v-model="opponentAddress" name="opponentAddress" id="opponentAddress" placeholder="0x..." />
+            <input type="text" v-model="opponentAddress" name="opponentAddress" id="opponentAddress"
+                placeholder="0x..." />
             <label for="betAmount">Bet Amount</label>
             <input type="number" v-model="betAmount" name="betAmount" id="betAmount" placeholder="Bet Amount" />
             <button type="submit">Create Game</button>
         </form>
+
+        <div v-if="inviteUrl"> 
+            <p>Game created successfully! Invite your opponent to join the game by sharing the following URL:</p>
+            <a :href="inviteUrl" target="_blank">{{ inviteUrl }}</a>
+        </div>
     </div>
 </template>
 
 <style scoped>
 #form-container {
-  display: flex;
-  justify-content: center;
+    display: flex;
+    justify-content: center;
 }
 
 form {
-  border: 1px solid gray;
-  width: 300px;
-  padding: 10px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 5px;
+    border: 1px solid gray;
+    width: 300px;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 5px;
 }
 
 input {
-  text-align: center;
+    text-align: center;
 }
 
 button {
-  margin-top: 10px;
+    margin-top: 10px;
 }
 </style>
